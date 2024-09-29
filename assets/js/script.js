3bfe8e8d0a3b33ae6b0d8be45e087469f3b270c9
Moahmmed-Taqiuddin <taqiuddin3821@gmail.com>
2024-09-29 18:35:55 +0530

    added some changes in skils

--- assets/js/script.js
+++ assets/js/script.js
@@ -71,7 +71,7 @@ document.addEventListener('visibilitychange',
 
 // <!-- typed js effect starts -->
 var typed = new Typed(".typing-text", {
-    strings: [ "Java developer","Frontend development", "web designing", "web development"],
+    strings: [ "Frontend development","Java developer","DevOps Engineer"],
     loop: true,
     typeSpeed: 50,
     backSpeed: 25,
